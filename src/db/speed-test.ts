cfbcc7faf2886df6c0b4fa4c5e2482ab1c3a064e
ethang <hello@ethang.email>
2025-10-23 13:31:43 -0500

    Refactor speed test logic: rename function, adjust indentation, and update TypeScript configuration.

--- src/db/speed-test.ts
+++ src/db/speed-test.ts
@@ -3,48 +3,44 @@ import {invoke} from "@tauri-apps/api/core";
 import {TestResult} from "../util/types.ts";
 
 type SpeedTestStore = DBSchema & {
-    'speed-test': {
-        key: string;
-        value: {
-            download_mbps: number;
-            upload_mbps: number;
-            latency_ms: number;
-            jitter_ms: number;
-            packet_loss_percent: number;
-            timestamp: number;
-        };
-        indexes: {
-            'by-timestamp': string;
-        };
-    }
+	'speed-test': {
+		key: string;
+		value: {
+			download_mbps: number;
+			upload_mbps: number;
+			latency_ms: number;
+			jitter_ms: number;
+			packet_loss_percent: number;
+			timestamp: number;
+		};
+		indexes: {
+			'by-timestamp': string;
+		};
+	}
 }
 
 const getSpeedTestDb = async () => {
-    return openDB<SpeedTestStore>('speed-test', 1, {
-        upgrade(db) {
-            db.createObjectStore('speed-test', {
-                keyPath: 'timestamp',
-                autoIncrement: false,
-            });
-        }
-    });
-}
-
-export const speedTest = async () => {
-    const db = await getSpeedTestDb();
-
-    const testResult = await invoke<TestResult>("run_full_speed_test");
-
-    await db.put('speed-test', {
-        ...testResult,
-        timestamp: Date.now(),
-    });
+	return openDB<SpeedTestStore>('speed-test', 1, {
+		upgrade(db) {
+			db.createObjectStore('speed-test', {
+				keyPath: 'timestamp',
+				autoIncrement: false,
+			});
+		}
+	});
 }
 
 export const runSpeedTest = async () => {
-    speedTest();
-    const db = await getSpeedTestDb();
-    const records = await db.getAll('speed-test');
+	const db = await getSpeedTestDb();
 
-    return records.reverse();
+	const testResult = await invoke<TestResult>("run_full_speed_test");
+
+	await db.put('speed-test', {
+		...testResult,
+		timestamp: Date.now(),
+	});
+
+	const records = await db.getAll('speed-test');
+
+	return records.reverse();
 }
